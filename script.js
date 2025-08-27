77370522f404b9c1cb5d4d5061aed155b494557b
IGNIT3-xD <md.imranali089@gmail.com>
2025-08-27 13:18:40 +0600

    Hearts and coins functionality done

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 document.getElementById('cards').addEventListener('click', function (e) {
-    
+
     if (e.target.className.includes('heart-btn')) {
         const heart = document.getElementById('heart');
-        const heartNumber = Number(heart.innerText) + 1;
+        let heartNumber = Number(heart.innerText) + 1;
         heart.innerText = heartNumber;
     }
 
-    // else if(e.target.className.includes('heart-btn')){
-    //     console.log("Heart");
-    // }
+    else if (e.target.className.includes('call-btn')) {
+        const coin = document.getElementById('coin');
+        const coinNumber = Number(coin.innerText);
+
+        const serviceName = e.target.parentNode.parentNode.children[1].children[0].innerText;
+        const serviceNumber = e.target.parentNode.parentNode.children[2].children[0].innerText;
+        
+
+        if (coinNumber <= 0) {
+            alert('You can not call right now')
+            return;
+        }
+
+        alert(`📞 Calling to ${serviceName} ${serviceNumber}...`)
+        let coinCount = coinNumber - 20;
+        coin.innerText = coinCount;
+    }
 })
